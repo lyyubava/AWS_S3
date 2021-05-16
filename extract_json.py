fb9c412f367b7214c6ed4ae00edf0a5b60b0207d
UbHTKcQh <lyyubava@gmail.com>
2021-05-17 01:56:01 +0300

    final stage

--- extract_json.py
+++ extract_json.py
@@ -1,53 +1,37 @@
 import config
 import os
 import json
-from db import DB
+from db import Movies, Songs, Apps
 import pymysql
-import re
-
-db = DB()
 
 
-def normalize(s: str) -> str:
-    space = r'\s'
-    norm = re.sub(space, '_', s)
-    print(norm)
-    norm = norm.lower()
-    pattern = r'[^_a-zA-Z]'
-    norm = re.sub(pattern, '', norm)
-
-    return norm
 
 
-def insert_data_into_tables():
+def is_awesome(name: str) -> bool:
+    if name.startswith('U'):
+        return True
+    return False
+
+
+def insert_data_into_tables(*tables):
     for filename in os.listdir(config.Configuration.directory):
-
         with open(os.path.join(config.Configuration.directory, filename), 'r') as f:
-
             data = json.load(f)
-
             for key in data:
-
-                if key['type'] == 'movie':
-                    try:
-                        original_title = key['data'].get('original_title')
-                        if not original_title:
+                for table in tables:
+                    if key['type'] == table.type:
+                        try:
+                            table.insert_data(key)
+                        except pymysql.err.DataError:
                             continue
-                        original_language = key['data'].get('original_language', None)
-                        budget = key['data'].get('budget', None)
-                        is_adult = key['data'].get('budget', None)
-                        release_date = key['data'].get('release_date', None)
-                        original_title_normalized = normalize(key['data']['original_title'])
-                        val = (original_title, original_language, budget, is_adult, release_date, original_title_normalized)
-
-                        db.insert_data_into_movies(val)
-
-                    except KeyError:
-                        continue
-
-                    except pymysql.err.DataError:
-                        continue
 
 
 
-insert_data_into_tables()
+if __name__ == '__main__':
+    table_songs = Songs()
+    table_apps = Apps()
+    table_movies = Movies()
+    table_movies.create_table_movies()
+    table_songs.create_table()
+    table_apps.create_table()
+    insert_data_into_tables(table_apps, table_songs, table_movies)
