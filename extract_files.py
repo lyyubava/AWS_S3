08c9c09d9a3c4b63917de81e306b5fffcab9c7f9
UbHTKcQh <lyyubava@gmail.com>
2021-05-17 03:08:08 +0300

    s

--- extract_files.py
+++ extract_files.py
@@ -4,10 +4,10 @@ from config import Configuration
 import os
 
 
-directory = config.Configuration.directory
-for key in data_list:
-    s3.download_file(Bucket=Configuration.BUCKET, Key=key,
-                     Filename=os.path.join(
-                         directory, key)
-                     )
-
+def extraction():
+    directory = config.Configuration.directory
+    for key in data_list:
+        s3.download_file(Bucket=Configuration.BUCKET, Key=key,
+                         Filename=os.path.join(
+                             directory, key)
+                         )
