e8340cd597eb6cf56e692ae700eba9c6b4b32640
Liubov <73103332+lyyubava@users.noreply.github.com>
2021-05-17 03:20:11 +0300

    Updated comfig

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ class Configuration:
     KEY = ''
     aws_secret_access_key = ''
     files_list_object_key = 'files_list.data'
-    output_filename = 'data_list'
-    directory = ''
+    output_filename = 'data_list' # don't even try to bother about name of output file =). You can change it also.
+    directory = '' # provide an absolute path for a directory, where you want to store all files
     password = ''
     db = ''
-    user = ''
+    user = '' # provide a name for database. It will be automatically created.
     host = ''
